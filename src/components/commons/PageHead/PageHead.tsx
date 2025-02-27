44bb7428ff9336df546716ad2ddbfef64ffaa9f0
ReymonRizz <reymond021ki@gmail.com>
2025-02-27 14:28:12 +0700

    feat: init project

---
+++ src/components/commons/PageHead/PageHead.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+
+
+interface PropTypes {
+    title?: string;
+}
+
+const PageHead = (props: PropTypes) => {
+    const { title = 'Tixly' } = props;
+    return(
+        <Head>
+            <title>{title}</title>
+            <meta charSet="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        </Head>
+    )
+}
+
+
+export default PageHead
